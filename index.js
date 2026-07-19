7c2215fcffb2c64b4eea9b8283fbfcd6c2f82cbf
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-07-20 00:46:29 +0600

    delete

--- index.js
+++ index.js
@@ -62,6 +62,13 @@ async function run() {
       
     })
 
+
+    app.delete('/destination/:id', async (req, res) => {
+      const { id } = req.params
+      const result = await destinationCollection.deleteOne({ _id: new ObjectId(id) })
+      res.send(result)
+    })
+
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
